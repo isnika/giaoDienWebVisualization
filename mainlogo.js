bf6a36e21ebcc815ff021c23ea66bce700f2e268
isnika <Ngkhanhhuyen67@gmail.com>
2025-10-02 22:45:57 +0700

    Update code

---
+++ mainlogo.js
@@ -0,0 +1,5 @@
+// Sau 30 giây tự động chuyển
+setTimeout(function () {
+    window.location.href = "../web2/Home/indexhome.html";
+    // đổi "index2.html" thành đường dẫn thực tế của bạn
+}, 5000); 
